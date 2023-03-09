59a145d63aa136fb7272984ca8d43b6971bdfdb9
adams <stilwelladam8@gmail.com>
2023-03-09 14:21:18 -0500

    Finished this lab.

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,88 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
-    private int age;
+    private String name, middleName, lastName;
+    private int age, heightInInch, numberOfKisses;
+
+    private boolean isAlive;
+
+
 
     public Person() {
+        name = "";
+        middleName = "";
+        lastName = "";
+        age = Integer.MAX_VALUE;
+        isAlive = true;
+        numberOfKisses = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        setAge(age);
     }
 
     public Person(String name) {
+        setName(name);
     }
 
-    public Person(String name, int age) {
+    public Person(String name, String middleName, String lastName, int age, int heightInInch, int numberOfKisses, boolean isAlive) {
+        setName(name);
+        setMiddleName(middleName);
+        setLastName(lastName);
+        setAge(age);
+        setHeightInInch(heightInInch);
+        setNumberOfKisses(numberOfKisses);
+        setAliveOrNot(isAlive);
     }
 
-    public void setName(String name) {
+    public boolean setAliveOrNot(boolean isAlive) {
+        return this.isAlive = isAlive;
     }
 
-    public void setAge(int age) {
+    public int setNumberOfKisses(int numberOfKisses) {
+        return this.numberOfKisses = numberOfKisses;
     }
 
+    public int setHeightInInch(int heightInInch) {
+        return this.heightInInch= heightInInch;
+    }
+
+    public String setName(String name) {
+        return this.name =name;
+    }
+    public String setMiddleName(String middleName) {
+        return this.middleName = middleName;
+    }
+    public String setLastName(String lastName) {
+        return this.lastName = lastName;
+    }
+    public int setAge(int age) {
+        return this.age = age;
+    }
     public String getName() {
-        return null;
+        return new String(name);
+    }
+
+    public String getMiddleName(){
+        return new String(middleName);
+    }
+
+    public String getLastName(){
+        return new String(lastName);
     }
 
     public Integer getAge() {
-        return null;
+        return new Integer(age);
+    }
+    public Integer getHeightInInch(){
+        return new Integer(heightInInch);
+    }
+
+    public Integer getNumberOfKisses(){
+        return new Integer(numberOfKisses);
+    }
+    public Boolean getIsAlive(){
+        return new Boolean(isAlive);
     }
 }
+
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,9 +12,14 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
+        String expectedMiddleName = "";
+        String expectedLastName = "";
+        Integer expectedHeightInInch = Integer.MAX_VALUE;
+        Integer expectedNumberOfKisses = Integer.MAX_VALUE;
+        Boolean expectedIsAlive = true;
 
         // When
-        Person person = new Person();
+        Person person = new Person(expectedName, expectedMiddleName, expectedLastName, expectedAge, expectedHeightInInch, expectedNumberOfKisses,  expectedIsAlive);
 
         // Then
         String actualName = person.getName();
@@ -52,20 +57,39 @@ public class TestPerson {
 
 
     @Test
-    public void testConstructorWithNameAndAge() {
+    public void testConstructorWithFullPersonClass() {
         // Given
         Integer expectedAge = 5;
         String expectedName = "Leon";
+        String expectedMiddleName = "S.";
+        String expectedLastName = "Kennedy";
+        Integer expectedNumberOfKisses = 2;
+        Integer expectedHeightInInch = 71;
+        Boolean expectedIsAlive = true;
 
         // When
-        Person person = new Person(expectedName, expectedAge);
+        Person person = new Person(expectedName, expectedMiddleName, expectedLastName, expectedAge, expectedHeightInInch,expectedNumberOfKisses, expectedIsAlive);
 
         // Then
         Integer actualAge = person.getAge();
         String actualName = person.getName();
+        String actualMiddleName = person.getMiddleName();
+        String actualLastName = person.getLastName();
+        Integer actualHeightInInch = person.getHeightInInch();
+        Integer actualNumberOfKissses = person.getNumberOfKisses();
+        Boolean actualIsAlive = person.getIsAlive();
+
 
         Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedMiddleName, actualMiddleName);
+        Assert.assertEquals(expectedLastName, actualLastName);
+        Assert.assertEquals(expectedHeightInInch, actualHeightInInch);
+        Assert.assertEquals(expectedNumberOfKisses, actualNumberOfKissses);
+        Assert.assertEquals(expectedIsAlive, actualIsAlive);
+
+
+
     }
 
     @Test
@@ -83,6 +107,45 @@ public class TestPerson {
     }
 
     @Test
+    public void testSetMiddleName() {
+        // Given
+        Person person = new Person();
+        String expected= "S.";
+
+        // When
+        person.setMiddleName(expected);
+
+        // Then
+        String actual = person.getMiddleName();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetLastName() {
+        // Given
+        Person person = new Person();
+        String expected = "Kennedy";
+
+        // When
+        person.setLastName(expected);
+
+        // Then
+        String actual = person.getLastName();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetHeightInInch() {
+        // Given
+        Person person = new Person();
+        Integer expected = 71;
+
+        // When
+        person.setHeightInInch(expected);
+
+        // Then
+        Integer actual = person.getHeightInInch();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
     public void testSetAge() {
         // Given
         Person person = new Person();
@@ -93,6 +156,32 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetNumberOfKisses() {
+        // Given
+        Person person = new Person();
+        Integer expected = 2;
+
+        // When
+        person.setNumberOfKisses(expected);
+
+        // Then
+        Integer actual = person.getNumberOfKisses();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetAliveOrNot() {
+        // Given
+        Person person = new Person();
+        Boolean expected = true;
+
+        // When
+        person.setAliveOrNot(expected);
+
+        // Then
+        Boolean actual = person.getIsAlive();
         Assert.assertEquals(expected, actual);
     }
 }
